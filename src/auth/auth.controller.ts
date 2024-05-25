c48ac33c83c1f16fe58c87afba229f3151fbb882
Hoang-Nguyen-Huy <hoangmegamind1302@gmail.com>
2024-05-25 15:04:09 +0700

    [EPM-14][HoangHN] feat: get profile with bearer auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, HttpCode, HttpStatus, Post, ValidationPipe } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Get, HttpCode, HttpStatus, Post, Request, UseGuards, ValidationPipe } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { Public } from './decorator/public.decorator';
 import { UsersDto } from 'src/dto/users.dto';
+import { AuthGuard } from './auth.guard';
 
 @ApiTags('Auth')
 @Controller('auth')
@@ -14,6 +15,13 @@ export class AuthController {
     @Post('login')
     signIn(@Body(new ValidationPipe()) userDto: UsersDto) {
         return this.authService.signIn(userDto.username, userDto.password);
+    }
+
+    @UseGuards(AuthGuard)
+    @Get('profile')
+    @ApiBearerAuth()
+    getProfile(@Request() req) {
+        return req.user;
     }
 
 }
