02844b6242bcee7dde3f93d7c5acd7ca8ecf648c
Hoang-Nguyen-Huy <hoangmegamind1302@gmail.com>
2024-05-29 00:26:32 +0700

    [EPM-14][HoangHN] refactor: format the reponse for the unauthorized

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,8 +3,9 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersHttpModule } from 'src/models/users/users-http.module';
 import { JwtModule } from '@nestjs/jwt';
-import { APP_GUARD } from '@nestjs/core';
+import { APP_FILTER, APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from './auth.guard';
+import { UnauthorizedExceptionFilter } from 'src/utils/unauthorized-exception.filter';
 
 @Module({
   imports: [
@@ -19,6 +20,10 @@ import { AuthGuard } from './auth.guard';
       provide: APP_GUARD,
       useClass: AuthGuard,
     },
+    {
+      provide: APP_FILTER,
+      useClass: UnauthorizedExceptionFilter,
+    }
   ]
 })
 export class AuthModule {}
