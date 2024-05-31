b05aaf376d05d591ee2fb80b7a9a24c4fa18a124
Hoang-Nguyen-Huy <hoangmegamind1302@gmail.com>
2024-05-31 21:58:01 +0700

    [EPM-9][HoangHN] feat: create clb feature and only admin can create clb

--- src/models/clbs/clbs.controller.ts
+++ src/models/clbs/clbs.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { ClbsService } from './clbs.service';
+import { ClbsDto } from 'src/dto/clbs.dto';
+import { ApiResponseDto } from 'src/utils/api-response.dto';
+import { Response } from 'express';
+import { Role } from 'src/enum/roles/role.enum';
+import { Roles } from 'src/enum/roles/role.decorator';
 
+@ApiTags('Clbs')
 @Controller('clbs')
-export class ClbsController {}
+export class ClbsController {
+    constructor (
+        private readonly clbsService: ClbsService
+    ) {};
+
+    @Roles(Role.Admin)
+    @Post('/create-clbs')
+    @ApiBearerAuth()
+    async createClub(@Body() clbsDto: ClbsDto, @Res() res: Response) {
+        const responseClbs = await this.clbsService.createClbs(clbsDto);
+        if (responseClbs === null) {
+            return res.status(403).json(new ApiResponseDto(responseClbs, 'Create clb fail'));
+        } else {
+            return res.status(201).json(new ApiResponseDto(responseClbs, 'Create clb successfully'));
+        }
+    }
+}
