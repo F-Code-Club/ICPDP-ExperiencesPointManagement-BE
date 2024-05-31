b85897148131663fea26d2d76d9b2c69495d91a9
Hoang-Nguyen-Huy <hoangmegamind1302@gmail.com>
2024-05-31 22:05:51 +0700

    [EPM-9][HoangHN] feat: handle exception for creating clb feature

--- src/dto/clbs.dto.ts
+++ src/dto/clbs.dto.ts
@@ -1,11 +1,9 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsNotEmpty } from "class-validator";
 
 export class ClbsDto {
     @ApiProperty({
         example: 'F-Code'
     })
-    @IsNotEmpty()
     name: string;
 
     avt?: string;
