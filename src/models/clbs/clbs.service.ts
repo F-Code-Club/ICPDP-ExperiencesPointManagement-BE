bcbb26b23b073d008b4e194a9389ea9c0b279ca2
Hoang-Nguyen-Huy <hoangmegamind1302@gmail.com>
2024-05-31 21:58:01 +0700

    [EPM-9][HoangHN] feat: create clb feature and only admin can create clb

--- src/models/clbs/clbs.service.ts
+++ src/models/clbs/clbs.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Clbs } from './clbs.entity';
+import { Repository } from 'typeorm';
+import { ClbsDto } from 'src/dto/clbs.dto';
 
 @Injectable()
-export class ClbsService {}
+export class ClbsService {
+    constructor (
+        @InjectRepository(Clbs)
+        private clbsRepository: Repository<Clbs>,
+    ) {};
+
+    /*
+    [POST]: create-clbs
+    */
+    async createClbs(clbsDto: ClbsDto): Promise<ClbsDto | null> {
+        const newClbs = this.clbsRepository.create(clbsDto);
+
+        if (!newClbs.avt) {
+            newClbs.avt = 'not set avt yet';
+        }
+
+        const savedClbs = await this.clbsRepository.save(newClbs);
+
+        return {
+            name: savedClbs.name,
+            avt: savedClbs.avt,
+        }
+    }
+}
