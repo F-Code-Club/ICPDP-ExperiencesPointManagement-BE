6bbe0a0529707f62cb0790534c3dade343e96eb5
Hoang-Nguyen-Huy <hoangmegamind1302@gmail.com>
2024-06-09 19:50:03 +0700

    [EPM-9][HoangHN] refactor: set cors to specific FE link

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ declare const module: any;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({ origin: ['*'] });
+  app.enableCors({ origin: 'http://localhost:5173' });
 
   const config = new DocumentBuilder()
     .addBearerAuth()
