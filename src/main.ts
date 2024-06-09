9e9177343201a5890f177486e150a893c3d8b1ab
Hoang-Nguyen-Huy <hoangmegamind1302@gmail.com>
2024-06-09 17:58:09 +0700

    [EPM-9][HoangHN] refactor: enable cors to all

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ declare const module: any;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({ origin: '*' });
+  app.enableCors({ origin: ['*'] });
 
   const config = new DocumentBuilder()
     .addBearerAuth()
