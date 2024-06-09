05e0cc48e54a7500d34b6d4c71ac227ef01f33a9
Hoang-Nguyen-Huy <hoangmegamind1302@gmail.com>
2024-06-09 23:36:14 +0700

    [EPM-9][HoangHN] refactor: set CORS

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,14 @@ declare const module: any;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({ 
-    origin: [
-      "http://localhost:5173"
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
+  app.enableCors({
+    origin: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    // allowed headers
+    allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'Authorization'],
+    // headers exposed to the client
+    exposedHeaders: ['Authorization'],
+    credentials: true, // Enable credentials (cookies, authorization headers) cross-origin
   });
 
   const config = new DocumentBuilder()
