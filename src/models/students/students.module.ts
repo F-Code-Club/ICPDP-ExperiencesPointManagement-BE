7dd72c61e158f2561b7fd65ac6c882d01d68bff7
Hoang-Nguyen-Huy <hoangmegamind1302@gmail.com>
2024-06-18 13:26:59 +0700

    [EPM-27][HoangHN] refactor: add app, roles guard and filter to student module

--- src/models/students/students.module.ts
+++ src/models/students/students.module.ts
@@ -3,11 +3,33 @@ import { StudentsController } from './students.controller';
 import { StudentsService } from './students.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Students } from './students.entity';
+import { APP_FILTER, APP_GUARD } from '@nestjs/core';
+import { RolesGuard } from 'src/enum/roles/role.guard';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { UnauthorizedExceptionFilter } from 'src/utils/unauthorized-exception.filter';
+import { BadRequestExceptionExceptionFilter } from 'src/utils/badrequest-exception.filter';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Students])],
   exports: [TypeOrmModule],
   controllers: [StudentsController],
-  providers: [StudentsService]
+  providers: [StudentsService,
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: BadRequestExceptionExceptionFilter,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: UnauthorizedExceptionFilter,
+    }
+  ]
 })
 export class StudentsModule {}
