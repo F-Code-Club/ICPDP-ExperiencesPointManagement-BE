0fb5206df357c95cb75a2b1bf19d37df3172ca5d
Hoang-Nguyen-Huy <hoangmegamind1302@gmail.com>
2024-06-25 15:51:53 +0700

    [EPM-29][HoangHN] refactor: explain the code of add student feature

--- src/models/eventPoint/event-point.service.ts
+++ src/models/eventPoint/event-point.service.ts
@@ -24,25 +24,29 @@ export class EventPointService {
     /* 
     [POST]: /eventpoint
     */
-    async addStudents (eventID, addStudentDto: EventPointCreateRequestDto, userRole: string, userId: string) {
+    async addStudents (eventID: string, addStudentDto: EventPointCreateRequestDto, userRole: string, userId: string) {
         let createdStudents = null;
 
+        // check valid of studentID
         const checkStudentID = await this.studentService.checkValidId(addStudentDto.studentID);
         if (!checkStudentID) {
             throw new ForbiddenException("ID must follow the standards of FPT University's student code");
         }
 
+        // check if this studentID is exist on this eventID or not
         const checkExistStudentOnThisEvent = await this.findByStudentID(addStudentDto.studentID);
         if (checkExistStudentOnThisEvent) {
             throw new ForbiddenException(`This student ${addStudentDto.studentID} is already exist on this event`);
         }
 
+        // check if this studentID is exist on this application or not
         const checkExistStudentOnSystem = await this.studentService.findByID(addStudentDto.studentID);
         if (!checkExistStudentOnSystem) {
             throw new ForbiddenException(`This student ${addStudentDto.studentID} is not exist on this application`);
         }
         addStudentDto.student = checkExistStudentOnSystem;
 
+        // check if this eventID is exist or not
         const checkEvent = await this.eventService.findById(eventID);
         if (!checkEvent) {
             throw new ForbiddenException('This event is not exist');
