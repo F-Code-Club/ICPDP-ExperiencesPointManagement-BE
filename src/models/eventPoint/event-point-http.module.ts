695e863ec74f857f6a9b900e1297a916b701eb90
Hoang-Nguyen-Huy <hoangmegamind1302@gmail.com>
2024-08-08 15:55:11 +0700

    [EPM-36][HoangHN] feat: import student to event point from excel

--- src/models/eventPoint/event-point-http.module.ts
+++ src/models/eventPoint/event-point-http.module.ts
@@ -10,9 +10,36 @@ import { RoleClbsHttpModule } from "../roleClbs/role-clbs-http.module";
 import { RoleDeparmentsHttpModule } from "../roleDepartments/role-departments-http.module";
 import { ClubMemberHttpModule } from "../clubMember/club-member-http.module";
 import { DepartmentMemberHttpModule } from "../departmentMember/department-member-http.module";
+import { LocalFilesModule } from "src/local-files/local-files.module";
+import * as fs from "fs";
+import * as multer from "multer";
+import { MulterModule } from "@nestjs/platform-express";
+
+const uploadsFolder = 'uploads';
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => { 
+        if(!fs.existsSync(uploadsFolder)) {
+            fs.mkdirSync(uploadsFolder);
+        }
+        cb(null, uploadsFolder);
+    },
+    filename: (req, file, cb) => {
+        const fileInfo = file.originalname.split(".");
+        let fileName = file.originalname;
+        let filePath = `${uploadsFolder}/${fileName}`;
+        let i = 1;
+        while(fs.existsSync(filePath)) {
+            fileName = fileInfo[0] + `(${i})` + (fileInfo.length > 1 ? "." + fileInfo[1] : "");
+            filePath = `${uploadsFolder}/${fileName}`;
+            i++;
+        }
+        cb(null, fileName);
+    }
+});
 
 @Module({
-    imports: [EventPointModule, EventHttpModule, ClbsHttpModule, DepartmentsHttpModule, StudentsHttpModule, RoleClbsHttpModule, RoleDeparmentsHttpModule, ClubMemberHttpModule, DepartmentMemberHttpModule],
+    imports: [MulterModule.register({ storage, limits: { fileSize: 8 * 1024 * 1024 } }), EventPointModule, EventHttpModule, ClbsHttpModule, DepartmentsHttpModule, StudentsHttpModule, RoleClbsHttpModule, RoleDeparmentsHttpModule, ClubMemberHttpModule, DepartmentMemberHttpModule, LocalFilesModule],
     controllers: [EventPointController],
     providers: [EventPointService],
     exports: [EventPointService]
