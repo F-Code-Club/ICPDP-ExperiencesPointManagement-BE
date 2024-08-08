a347ba59046528f154de8e28ed1e6a9660b72384
Hoang-Nguyen-Huy <hoangmegamind1302@gmail.com>
2024-08-08 22:21:27 +0700

    [EPM-36][HoangHN] refactor: enable CORS

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,8 @@ async function bootstrap() {
       "http://localhost:5173/",
       "http://localhost:5174",
       "http://localhost:5174/",
-      "https://epm-fe-dev.f-code.tech"
+      "https://epm-fe-dev.f-code.tech",
+      "https://epm-fe-dev.f-code.tech/"
     ],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     // allowed headers
