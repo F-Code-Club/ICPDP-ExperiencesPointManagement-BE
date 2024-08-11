41b617349aec206c51a6c0dc77388abe7f4185c2
Hoang-Nguyen-Huy <hoangmegamind1302@gmail.com>
2024-08-11 22:26:38 +0700

    [EPM-36][HoangHN] refactor: user can not login when organization not active

--- src/models/users/users.module.ts
+++ src/models/users/users.module.ts
@@ -3,9 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Users } from './users.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { Clbs } from '../clbs/clbs.entity';
+import { Departments } from '../departments/departments.entity';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Users])],
+    imports: [TypeOrmModule.forFeature([Users, Clbs, Departments])],
     exports: [TypeOrmModule],
     controllers: [UsersController],
     providers: [UsersService]
