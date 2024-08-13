762990a8e7a327716f26bdcbc65cda718c24fde9
Hoang-Nguyen-Huy <hoangmegamind1302@gmail.com>
2024-08-13 12:13:18 +0700

    [EPM-36][HoangHN] fix: fix error of semester's date

--- src/models/final-point/final-point.module.ts
+++ src/models/final-point/final-point.module.ts
@@ -5,10 +5,11 @@ import { FinalPointController } from './final-point.controller';
 import { FinalPointService } from './final-point.service';
 import { EventPoint } from '../eventPoint/event-point.entity';
 import { Students } from '../students/students.entity';
+import { Semesters } from '../semesters/semesters.entity';
 
 
 @Module({
-  imports: [TypeOrmModule.forFeature([FinalPoint, EventPoint, Students])],
+  imports: [TypeOrmModule.forFeature([FinalPoint, EventPoint, Students, Semesters])],
   exports: [TypeOrmModule],
   controllers: [FinalPointController],
   providers: [FinalPointService]
